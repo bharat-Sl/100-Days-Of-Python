ad16119b06860e1d8566aa9938c6169ad5cc5730
bharat-Sl <bharatag00@gmail.com>
2020-12-31 00:36:28 +0530

    new commit

---
+++ Day-10/Challenge1.py
@@ -0,0 +1,8 @@
+#challenge 1
+
+
+def format_name(f_name,l_name):
+    full_name=f_name+' '+l_name
+    return full_name.title()
+
+print(format_name('BhaRaT','AgArWaL'))
---
+++ Day-10/Day10_Project.py
@@ -0,0 +1,72 @@
+#Project Day3
+
+#clear terminal
+#---------------------------------------------
+import os
+def clear():
+    os.system('cls')
+#---------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+#------------------------------------------------------------------------------------------------------------
+
+def add(num1,num2):
+    return num1+num2
+
+def subtract(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
+
+def calc(num1,num2,operation):
+    s=0.0
+    if(operation=='+'):
+        s=add(num1,num2)
+    elif(operation=='-'):
+        s=subtract(num1,num2)
+    elif(operation=='*'):
+        s=multiply(num1,num2)
+    else:
+        s=divide(num1,num2)
+        s=round(s,3)
+    print(f'{num1} {operation} {num2} = {s}')
+    return s
+
+operands=['+','-','*','/']
+first_num=float(input("What's the first number?: "))
+print('+\n-\n*\n/')
+end=False
+while not end:
+    operation=input("Pick an operation: ")
+    if(operation not in operands):
+        print('Operand not recognised. Please re-enter.')
+        continue
+    second_num=float(input("What's the next number?: "))
+    first_num=calc(first_num,second_num,operation)
+
+    choice=input(f"Type 'y' to continue calculating with {first_num},\nor type 'n' to start a new calculation,\nor type 'exit' to exit: ")
+    if(choice.lower()=='n'):
+        clear()
+        first_num=float(input("What's the first number?: "))
+    elif(choice.lower()=='exit'):
+        end=True
